dfc2b9a988d8a7e0105deeace41e0c35465f9fc2
Anas <109545112+anousselam@users.noreply.github.com>
2023-01-15 17:01:09 +0100

    Add files via upload

---
+++ Views/Home/cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dotnet6MvcLogin.Views.Home
+{
+    public class cartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
