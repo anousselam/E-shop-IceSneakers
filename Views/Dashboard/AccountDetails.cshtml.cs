7e21caf4c43a35ce0d53a97db60ada238d988adb
Anas <109545112+anousselam@users.noreply.github.com>
2023-01-15 17:00:05 +0100

    Add files via upload

---
+++ Views/Dashboard/AccountDetails.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dotnet6MvcLogin.Models;
+using Dotnet6MvcLogin.Models.Domain;
+using Dotnet6MvcLogin.Models.DTO;
+namespace Dotnet6MvcLogin.Views.Home
+{
+    public class AccountDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+           
+            
+        }
+    }
+}
